780a7288cfa8f3160d5cb9291fe2667f5f1e6171
Evan Cook <162135293+evantcook1@users.noreply.github.com>
2025-02-23 13:29:44 -0500

    Attempting to resolve deploy errors

--- next.config.js
+++ next.config.js
@@ -1,23 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { dev, isServer }) => {
-    // Optimize webpack caching
-    config.cache = {
-      type: 'filesystem',
-      buildDependencies: {
-        config: [__filename],
-      },
-      cacheDirectory: '.next/cache/webpack',
-      name: isServer ? 'server' : 'client',
-      version: '1.0.0',
-    };
-
-    return config;
-  },
-  // Enable streaming responses
-  experimental: {
-    serverActions: true,
-  },
   // Optimize image handling
   images: {
     domains: ['*'],
